ebaea3bbc904dc757cb884c0243d1535f7abf82b
Sardakan <sardakan1991@gmail.com>
2026-05-18 22:26:08 +0800

    Fix: support Russian field names in smart bulk update

--- sync_microservices/processor.js
+++ sync_microservices/processor.js
@@ -497,9 +497,12 @@ const syncProcessor = {
 				"salePrices": () => {
 					payload.priceCurrent = product.salePrices ? product.salePrices[0].value / 100 : null;
 					payload.priceOld = product.salePrices && product.salePrices[1] ? product.salePrices[1].value / 100 : null;
+				},
+				"Цена продажи (сайт)": () => {
+					payload.priceCurrent = product.salePrices ? product.salePrices[0].value / 100 : null;
+					payload.priceOld = product.salePrices && product.salePrices[1] ? product.salePrices[1].value / 100 : null;
 				}
 			};
-
 			// Маппинг дополнительных атрибутов
 			const attrMap = {
 				"brand": "brand",
