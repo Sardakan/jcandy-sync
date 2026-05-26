028e3cd9d5a53a399cd011463006ebe922c1b29b
Sardakan <sardakan1991@gmail.com>
2026-05-26 18:53:21 +0800

    refresh ID

--- sync_microservices/config.js
+++ sync_microservices/config.js
@@ -11,15 +11,14 @@ const CONFIG = {
 	SYNC_DELAY: 3000,
 	MS_API_Token: process.env.MS_API_TOKEN,
 	ORDER_STATES: {
-		pending: "https://api.moysklad.ru/api/remap/1.2/entity/customerorder/metadata/states/c99deaaf-4619-11f1-0a80-1ba10025a79c",
-		processing: "https://api.moysklad.ru/api/remap/1.2/entity/customerorder/metadata/states/c99debb6-4619-11f1-0a80-1ba10025a79d",
-		shipped: "https://api.moysklad.ru/api/remap/1.2/entity/customerorder/metadata/states/c99dec5b-4619-11f1-0a80-1ba10025a79e",
-		delivered: "https://api.moysklad.ru/api/remap/1.2/entity/customerorder/metadata/states/c99dece6-4619-11f1-0a80-1ba10025a79f",
-		cancelled: "https://api.moysklad.ru/api/remap/1.2/entity/customerorder/metadata/states/c99dee1f-4619-11f1-0a80-1ba10025a7a1",
+		pending: "https://api.moysklad.ru/api/remap/1.2/entity/customerorder/metadata/states/99574bc9-58e1-11f1-0a80-1ca50045e5bb",
+		processing: "https://api.moysklad.ru/api/remap/1.2/entity/customerorder/metadata/states/99574c18-58e1-11f1-0a80-1ca50045e5bc",
+		shipped: "https://api.moysklad.ru/api/remap/1.2/entity/customerorder/metadata/states/99574c77-58e1-11f1-0a80-1ca50045e5bd",
+		delivered: "https://api.moysklad.ru/api/remap/1.2/entity/customerorder/metadata/states/99574cc0-58e1-11f1-0a80-1ca50045e5be",
+		cancelled: "https://api.moysklad.ru/api/remap/1.2/entity/customerorder/metadata/states/99574d50-58e1-11f1-0a80-1ca50045e5c0",
 	},
-	ORGANIZATION_HREF: "https://api.moysklad.ru/api/remap/1.2/entity/organization/c98a7ea4-4619-11f1-0a80-1ba10025a766",
-	STORE_HREF: "https://api.moysklad.ru/api/remap/1.2/entity/store/c98c0884-4619-11f1-0a80-1ba10025a769",
-	SERVICE_DELIVERY_HREF: "https://api.moysklad.ru/api/remap/1.2/entity/service/efd419d8-461d-11f1-0a80-1038002646af",
+	ORGANIZATION_HREF: "https://api.moysklad.ru/api/remap/1.2/entity/organization/99449ce2-58e1-11f1-0a80-1ca50045e57e",
+	STORE_HREF: "https://api.moysklad.ru/api/remap/1.2/entity/store/99461001-58e1-11f1-0a80-1ca50045e586",
 	// Ссылки на типы цен (МойСклад)
 	PRICE_TYPE_REGULAR: "9946a728-58e1-11f1-0a80-1ca50045e58d", // Цена продажи (сайт)
 	PRICE_TYPE_OLD: "a9b3389f-58e3-11f1-0a80-076a0045984b",     // Старая цена (сайт)
